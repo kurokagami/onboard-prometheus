413a3cd1412af2b496e13f957009af9edb8eacb5
BlackMirror <43965149+Guilldo@users.noreply.github.com>
2024-09-12 14:20:12 -0300

    Adicionando Branch Do Framework

--- data/general-script.js
+++
@@ -1,9 +0,0 @@
-function redirecionarProj() {
-    // Define a URL projetos
-    window.location.href = './projetos.php';
-  }
-
-  function redirecionarInd() {
-    // Define a URL index
-    window.location.href = './index.php';
-  }
